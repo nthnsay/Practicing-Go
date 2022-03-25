3a748325cd6752e599535ece551a1147e5deb692
abir1997 <s3677701@student.rmit.edu.au>
2022-03-25 18:24:01 +1100

    Add WIP

--- task-cli/main.go
+++ task-cli/main.go
@@ -7,7 +7,27 @@ import (
 	//"flag"
 )
 
+var tasks []Task
+
+const (
+	AddTask   string = "add"
+	DoTask    string = "do"
+	ListTasks string = "list"
+)
+
+func Init() {
+	tasks = []Task{
+		{
+			TaskId:      1,
+			IsCompleted: false,
+			Description: "Eat cookies",
+		},
+	}
+}
+
+//TODO: add commmand structs
 func main() {
+	Init()
 	greet := `task is a CLI for managing your TODOs.
 
 	Usage:
@@ -23,4 +43,29 @@ func main() {
 	input := bufio.NewScanner(os.Stdin)
 	input.Scan()
 	fmt.Println(input.Text())
+	fmt.Println(tasks)
+
+}
+
+func HandleInput(inputStr string) {
+	switch inputStr {
+	case AddTask:
+		//Add()
+	}
+}
+
+type Task struct {
+	TaskId      int
+	IsCompleted bool `default:false`
+	Description string
+}
+
+func Add(description string) {
+	t := Task{
+		TaskId:      len(tasks) + 1,
+		Description: description,
+	}
+	tasks = append(tasks, t)
+	fmt.Println(tasks)
+	// TODO return sth / error handling
 }
